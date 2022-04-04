51228355c0d4c38d372e4a245dd97872692ad432
Mashadasha23 <97893139+Mashadasha23@users.noreply.github.com>
2022-04-04 18:52:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+Canvas = document.getElementById("my_canvas");
+
+pen = Canvas.getContext("2d");
+
+
+
+var last_pos_x,last_pos_y; 
+
+color = "purple";
+width_line = 5;
+
+Canvas.addEventListener("touchstart",my_touch_start);
+function my_touch_start(e) {
+
+        last_pos_x = e.touches[0].clientX-Canvas.offsetLeft;
+        last_pos_y= e.touches[0].clientY-Canvas.offsetTop;
+   
+}
+
+
+
+Canvas.addEventListener("touchmove",my_touchMove);
+function my_touchMove(e){
+     
+      current_pos_x=e.touches[0].clientX-Canvas.offsetLeft;
+      current_pos_y=e.touches[0].clientY-Canvas.offsetTop;
+
+        pen.beginPath();
+        pen.strokeStyle = color;
+        pen.lineWidth = width_line;
+        pen.moveTo(last_pos_x,last_pos_y);   
+        pen.lineTo(current_pos_x,current_pos_y);
+        pen.stroke();
+        last_pos_x = current_pos_x;
+        last_pos_y = current_pos_y;
+    
+}
+
